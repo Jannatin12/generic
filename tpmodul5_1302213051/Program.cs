90ff44308609c8641157fe63c27734c3ff071e31
Jannatin12 <jannatinnurrohmah@gmail.com>
2023-03-19 15:28:32 +0700

    Add DataGeneric Class

--- tpmodul5_1302213051/Program.cs
+++ tpmodul5_1302213051/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using tpmodul5_1302213051;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        DataGeneric<string> datgen = new DataGeneric<string>("1302213051");
+        datgen.PrintData();
+    }
+}
