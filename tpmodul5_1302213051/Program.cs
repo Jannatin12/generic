e414d452ec1ecb1e6d47aae0f3ec62de29d9e990
Jannatin12 <jannatinnurrohmah@gmail.com>
2023-03-19 16:15:13 +0700

    update

--- tpmodul5_1302213051/Program.cs
+++ tpmodul5_1302213051/Program.cs
@@ -7,12 +7,13 @@ public class Program
 {
     public static void Main(string[] args)
     {
-<<<<<<< HEAD
+        //Generic Method
+        /*HaloGeneric hay = new HaloGeneric();
+        hay.SapaUser("Jannah");*/
+
+        //Generic Class
         DataGeneric<string> datgen = new DataGeneric<string>("1302213051");
         datgen.PrintData();
-=======
-        HaloGeneric hay = new HaloGeneric();
-        hay.SapaUser("Jannah");
->>>>>>> 4322f6fd5c6e7b2eaefb5c0e403e3eb169ea6c6d
+
     }
 }
